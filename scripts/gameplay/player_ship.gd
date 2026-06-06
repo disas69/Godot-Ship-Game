984e28b6084b6b5a5016a538d29e43f3627b0551
Eugene <evgenii.esaulenko@gmail.com>
2026-06-06 17:02:16 +0200

    Improve menu

--- scripts/gameplay/player_ship.gd
+++ scripts/gameplay/player_ship.gd
@@ -3,6 +3,11 @@ class_name PlayerShip extends Ship
 @export var local_player_index: int = 0
 @export var control_scheme: String = PlayerInput.CONTROL_KEYBOARD
 @export var allow_mouse_aim: bool = true
+@export var ignore_input: bool = false:
+	set(value):
+		ignore_input = value
+		if ignore_input:
+			disable_input_visuals()
 @export_range(0.0, 0.2, 0.01) var gamepad_aim_release_snap_guard_duration: float = 0.08
 @export_range(0.0, 0.5, 0.01) var gamepad_aim_release_snap_window: float = 0.12
 @export_range(0.0, 1.0, 0.01) var gamepad_aim_release_snap_min_previous_strength: float = 0.85
@@ -17,11 +22,18 @@ var gamepad_aim_release_snap_time_left := 0.0
 
 
 func _ready() -> void:
+	if ignore_input:
+		show_aim_helpers = false
 	player_input.init_actions(local_player_index, control_scheme)
 	super._ready()
+	if ignore_input:
+		disable_input_visuals()
 
 
 func _input(event: InputEvent) -> void:
+	if ignore_input:
+		return
+
 	if control_scheme == PlayerInput.CONTROL_KEYBOARD and (event is InputEventKey or event is InputEventMouse):
 		is_using_gamepad = false
 	elif is_selected_gamepad_event(event):
@@ -42,14 +54,20 @@ func is_selected_gamepad_event(event: InputEvent) -> bool:
 
 
 func get_move_input() -> Vector2:
+	if ignore_input:
+		return Vector2.ZERO
 	return player_input.get_input_vector("move_left", "move_right", "move_up", "move_down")
 
 
 func get_aim_input() -> Vector2:
+	if ignore_input:
+		return Vector2.ZERO
 	return player_input.get_input_vector("aim_left", "aim_right", "aim_up", "aim_down")
 
 
 func get_gamepad_aim_position(aim: Vector2, delta: float) -> Variant:
+	if ignore_input:
+		return null
 	return super.get_gamepad_aim_position(filter_gamepad_aim_release_snap(aim, delta), delta)
 
 
@@ -90,16 +108,32 @@ func is_gamepad_aim_release_snap(aim: Vector2, aim_strength: float, last_strengt
 
 
 func should_use_mouse_aim(aim_input: Vector2) -> bool:
+	if ignore_input:
+		return false
 	return allow_mouse_aim and control_scheme == PlayerInput.CONTROL_KEYBOARD and aim_input.is_zero_approx() and not is_using_gamepad
 
 
 func should_keep_gamepad_aim_without_input() -> bool:
+	if ignore_input:
+		return false
 	return is_using_gamepad
 
 
 func can_shoot() -> bool:
+	if ignore_input:
+		return false
 	var action: StringName = player_input.get_action("shoot")
 	return action != StringName("") and Input.is_action_just_pressed(action)
+
+
+func disable_input_visuals() -> void:
+	show_aim_helpers = false
+	auto_aim_target_ship = null
+	auto_aim_target = Vector3.ZERO
+	if aim_line != null:
+		aim_line.visible = false
+	if aim_indicator != null:
+		aim_indicator.visible = false
 
 
 func on_shot_fired() -> void:
